6f7795199eea0e2ea50941a8abec58a3806eebc6
DkXLed <derekleekent@gmail.com>
2025-07-10 21:12:35 -0500

    created function assigning choice by random number

--- rps.js
+++ rps.js
@@ -1,2 +1,16 @@
 //RPS
 
+function getComputerChoice() { 
+    const randomNumber = Math.random();
+
+    if (randomNumber <=.33) {
+        return "Rock" ;
+    }
+    else if (randomNumber >= .34 && randomNumber <= .66) {
+        return "Paper" ;
+    }
+    else return "Scissors" ;
+    
+}
+
+console.log(getComputerChoice())
