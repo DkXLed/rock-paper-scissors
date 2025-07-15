ce7bef6535c7f7d1b79a2852f17a75f8a1f303c3
DkXLed <derekleekent@gmail.com>
2025-07-14 21:28:34 -0500

    faulty code for playRound

--- rps.js
+++ rps.js
@@ -5,12 +5,13 @@ function getComputerChoice() {
     const randomNumber = Math.random();
 
     if (randomNumber <=.33) {
-        return "Computer threw Rock" ;
+        return "rock" ;
     }
     else if (randomNumber >= .34 && randomNumber <= .66) {
-        return "Computer threw Paper" ;
-    }
-    else return "Computer threw Scissors" ;
+        return "paper" ;
+    } else {
+        return "scissors" ;
+        }
     
 }
 
@@ -18,15 +19,47 @@ function getHumanChoice() {
     
     let handThrow = window.prompt("Rock, Paper, or Scissors? Choose Wisely!").toLowerCase();
     if (handThrow==="rock") {
-        return("You threw Rock") ;
+        return "rock" ;
     }
     else if (handThrow==="paper") {
-        return("You threw Paper") ;
+        return "paper" ;
     }
     else {
-        return("You threw Scissors") ;
+        return "scissors" ;
     }
 }
 
 console.log(getHumanChoice())
 console.log(getComputerChoice())
+
+function playRound(humanChoice , computerChoice) {
+    
+    if ( humanSelection === computerSelection) {
+        return("Tie, try again");
+    }
+    else if(humanSelection === "rock" && computerSelection === "scissors"){
+        return("Rock beats scissors! Take that, Chip") ;
+    }
+    else if (humanSelection === "rock" && computerSelection === "paper") {
+        return("Paper beats rock! The machines are learning your moves.") ;
+    }
+    else if (humanSelection === "paper" && computerSelection === "scissors") {
+        return("Scissors beats paper! Can the machine read your thoughts?");
+    }
+    else if (humanSelection === "paper" && computerSelection === "rock") {
+        return("Paper beats rock! We covered that in a previous section.");
+    }
+    else if (humanSelection === "scissors" && computerSelection === "paper") {
+        return("Scissors beats paper! Cuts like a knife!") ;
+    }
+    else if (humanSelection === "scissors" && computerSelection === "rock") {
+        return("Rock beats scissors! Crushing, isn't it?") ;
+    }
+}
+
+const humanSelection = getHumanChoice() ;
+const computerSelection = getComputerChoice() ;
+
+playRound(humanSelection , computerSelection) ;
+
+console.log(playRound(humanSelection, computerSelection))
