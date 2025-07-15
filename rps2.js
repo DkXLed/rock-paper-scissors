7e807ec37176886d89c3adcbb6033ab3fd0ec671
DkXLed <derekleekent@gmail.com>
2025-07-14 21:41:15 -0500

    created second rps.js file to restart script. created computer choice function

---
+++ rps2.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    const randomNumber = Math.random();
+    if (randomNumber <= .33) {
+        return "rock" ;
+    } else if (randomNumber <= .66) {
+        return "paper" ;
+    } else {
+        return "scissors" ;
+    }
+}
+
+console.log(getComputerChoice())
+
+function getHumanChoice() {
+    let 
+}
