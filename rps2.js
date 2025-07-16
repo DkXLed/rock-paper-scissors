41bd847daa821feb8f0a91dc05dc7014553b99b0
DkXLed <derekleekent@gmail.com>
2025-07-15 22:30:59 -0500

    fixed playRound function by logging human and comp selection within function

--- rps2.js
+++ rps2.js
@@ -2,6 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 // function to have computer choose 
+
 function getComputerChoice() {
     const randomNumber = Math.random();
     if (randomNumber <= .33) {
@@ -15,12 +16,34 @@ function getComputerChoice() {
 
 // function to prompt human to choose
 function getHumanChoice() {
-    let humanChoice = window.prompt("Rock, Paper, or Scissors? Choose wisely!").toLowerCase() ;
-    return humanChoice;
+    let yourRpsPick = window.prompt("Rock, Paper, or Scissors? Choose wisely!").toLowerCase() ;
+    return yourRpsPick;
+}
+function playRound(humanChoice, computerChoice) {
+    console.log("You picked " + humanSelection)
+    console.log("Computer picked " + computerSelection)
+    if (humanChoice === computerChoice) {
+        return "Tie, throw hands again!";
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock" || 
+             humanChoice === "rock" && computerChoice === "scissors" ||
+             humanChoice === "scissors" && computerChoice === "paper") {
+        return "Good throw. Point goes to you!";
+        }
+    else (humanChoice === "paper" && computerChoice === "scissors" ||
+          humanChoice === "rock" && computerChoice === "paper" ||
+          humanChoice === "scissors" && computerChoice === "rock") 
+        return "Poor choice. Computer wins this round";
+        
 }
 
-console.log("You chose " + getHumanChoice())
-console.log("Computer chose " + getComputerChoice())
+const humanSelection = getHumanChoice();
+const computerSelection =getComputerChoice();
+console.log(playRound(humanSelection, computerSelection)) ;
+
+
+/*console.log("You picked " + getHumanChoice());
+console.log("Computer chose " + getComputerChoice())*/
 
 
 
